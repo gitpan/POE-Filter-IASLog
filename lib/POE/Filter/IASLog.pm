d3a3d711492490e4c4da1b2c687e904a85ffa26b
Chris Williams <chris@bingosnet.co.uk>
2008-05-05 07:45:28 +0000

    Import of BINGOS/POE-Filter-IASLog-1.04 from CPAN.
    gitpan-cpan-distribution: POE-Filter-IASLog
    gitpan-cpan-version:      1.04
    gitpan-cpan-path:         BINGOS/POE-Filter-IASLog-1.04.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- lib/POE/Filter/IASLog.pm
+++ lib/POE/Filter/IASLog.pm
@@ -6,7 +6,7 @@ use Parse::IASLog;
 use base qw(POE::Filter);
 use vars qw($VERSION);
 
-$VERSION = '1.02';
+$VERSION = '1.04';
 
 sub new {
   my $class = shift;
@@ -116,6 +116,12 @@ Makes a copy of the filter, and clears the copy's buffer.
 =head1 AUTHOR
 
 Chris C<BinGOs> Williams <chris@bingosnet.co.uk>
+
+=head1 LICENSE
+
+Copyright C<(c)> Chris Williams
+
+This module may be used, modified, and distributed under the same terms as Perl itself. Please see the license that came with your Perl distribution for details.
 
 =head1 SEE ALSO
 
